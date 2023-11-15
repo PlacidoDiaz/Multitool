73c304d675e6577dd26c181db0b6148c71c59622
Placido <placidodiazgarrido@gmail.com>
2023-11-15 10:17:00 +0100

    Update Disabled/Enabled dnt work

--- popup/popup.js
+++ popup/popup.js
@@ -1,19 +1,65 @@
+/* Problemas para desactivar el adblock y recargar la página */
+
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("btn1").addEventListener("click", disableButton);
-  });
-  
+    document.getElementById("btn1").addEventListener("click", toggleAdBlock);
+    checkAdBlockStatus();
+});
 
-function disableButton() {
+function toggleAdBlock() {
+    var button = document.getElementById("btn1");
+    var buttonText = button.innerHTML;
 
-    var disableButton = document.getElementById("btn1").innerHTML;
+    console.log(buttonText);
+
+    if (buttonText === "Disabled") {
+        // Deshabilitar AdBlock
+        
+        chrome.storage.sync.set({ adBlockEnabled: false }, function() {
+            if (chrome.runtime.lastError) {
+                console.log("Error al guardar en storage:", chrome.runtime.lastError);
+            } else {
+                console.log("AdBlock Disabled");
+                button.innerHTML = "Disabled";
+                // Continuar con la lógica posterior
+            }
+        });
+        
+        /*chrome.storage.sync.set({ adBlockEnabled: false }, function() {
+            button.innerHTML = "Enabled";
+            location.reload();
+            console.log("AdBlock Disabled");
+        });*/
+        
+    } else if (buttonText === "Enabled") {
     
-    if (disableButton == "Disable") {
-        document.getElementById("btn1").innerHTML = "Enable";
-        console.log("Enable")
-    } else if (disableButton == "Enable") {
-        document.getElementById("btn1").innerHTML = "Disable";
-        console.log("Disable")
+        chrome.storage.sync.set({ adBlockEnabled: true }, function() {
+            if (chrome.runtime.lastError) {
+                console.log("Error al guardar en storage:", chrome.runtime.lastError);
+            } else {
+                console.log("AdBlock Enabled");
+                button.innerHTML = "Enabled";
+                // Continuar con la lógica posterior
+            }
+        });
+    
+        // Habilitar AdBlock
+        /*chrome.storage.sync.set({ adBlockEnabled: true }, function() {
+            button.innerHTML = "Disable";
+            location.runtime.reload()
+            console.log("AdBlock Enabled");
+        });*/
     }
-    
+}
+
+function checkAdBlockStatus() {
+    /*chrome.storage.sync.get('adBlockEnabled', function(data) {
+        var isEnabled = data.adBlockEnabled;
+        var button = document.getElementById("btn1");
+        if (isEnabled) {
+            button.innerHTML = "Disabled";
+        } else {
+            button.innerHTML = "Enabled";
+        }
+    });*/
 }
